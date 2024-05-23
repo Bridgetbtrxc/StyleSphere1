477ef99409e5b841b58a2562196a85f431af5e61
bridgetbtrxc <bbeatrix@student.ciputra.ac.id>
2024-05-23 20:11:40 +0700

    Initial Commit

---
+++ StyleSphere/StyleSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleSphereApp.swift
+//  StyleSphere
+//
+//  Created by MacBook Pro on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
