1c7401f747cff37fe6753311a769242b5dc26496
bridgetbtrxc <bbeatrix@student.ciputra.ac.id>
2024-05-29 00:20:34 +0700

    Working on wardrobe view
    There is some error, but still in consideration for the database scheme, and the modelling  as well as making the UI dynamic. PROGRESS TO NOTE : only the headline & divider

--- StyleSphere/HomePage/View/WardrobeView.swift
+++ StyleSphere/HomePage/View/WardrobeView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct WardrobeView: View {
+    let data: [String] = ["Shirt", "Pants", "Dress", "Jacket"]
+    
     var body: some View {
-        Text("Wardrobe View")
+        VStack{
+            Spacer().frame(height:20)
+            Text("Wardrobe")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(Color.subColor)
+            Spacer().frame(height: 40)
+            Divider()
+            
+            Spacer()
+            
+            ScrollView {
+                        LazyVStack(spacing: 20) {
+                            ForEach(data.indices, id: \.self) { index in
+                                let imageIndex = index % 2
+                                let imageName = "Wardrobe\(imageIndex + 1)"
+                                let clothingType = data[index]
+                                
+                                ClothingItemView(imageName: imageName, clothingType: clothingType)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+            
+            
+            
+        }
     }
 }
 
