18a78b619555a901e2f8fd51bda604718b64ab52
Nessaa16 <vpriscilia@student.ciputra.ac.id>
2024-05-29 09:13:11 +0700

    adding the view of AddClothes but it's not finished yet

---
+++ StyleSphere/HomePage/View/AddClothes.swift
@@ -0,0 +1,18 @@
+//
+//  AddClothes.swift
+//  StyleSphere
+//
+//  Created by MacBook Pro on 29/05/24.
+//
+
+import SwiftUI
+
+struct AddClothes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddClothes()
+}
