cebb253ea33e4a3eddbd1a0961773b3518fbb381
Nessaa16 <vpriscilia@student.ciputra.ac.id>
2024-05-30 16:40:26 +0700

    calender view is done with the logic where it can automatically update to the latest day or month

--- StyleSphere/HomePage/View/CalendarView.swift
+++ StyleSphere/HomePage/View/CalendarView.swift
@@ -19,7 +19,7 @@ struct CalendarView: View {
                 VStack(spacing: 20) {
                     HStack {
                         Spacer()
-                        Text("2024")
+                        Text(extraDate()[0])
                             .font(.system(size: 18, weight: .semibold))
                             .foregroundColor(Color(red: 0.42, green: 0.31, blue: 0.22))
                         Spacer()
@@ -27,7 +27,7 @@ struct CalendarView: View {
                     
                     HStack {
                         Spacer()
-                        Text("May")
+                        Text(extraDate()[1])
                             .font(.system(size: 24, weight: .bold))
                             .foregroundColor(Color(red: 0.42, green: 0.31, blue: 0.22))
                         Spacer()
@@ -36,7 +36,9 @@ struct CalendarView: View {
                     HStack {
                         Spacer()
                         Button {
-                            // Action for left button
+                            withAnimation {
+                                currentMonth -= 1
+                            }
                         } label: {
                             Image(systemName: "chevron.left")
                                 .font(.title2)
@@ -46,7 +48,9 @@ struct CalendarView: View {
                         Spacer()
                         
                         Button {
-                            // Action for right button
+                            withAnimation {
+                                currentMonth += 1
+                            }
                         } label: {
                             Image(systemName: "chevron.right")
                                 .font(.title2)
@@ -71,25 +75,67 @@ struct CalendarView: View {
                     
                     LazyVGrid(columns: columns, spacing: 15) {
                         ForEach(extractDate()) { value in
-                            Text("\(value.day)")
-                                .font(.title3.bold())
-                                .foregroundColor(Color(red: 0.42, green: 0.31, blue: 0.22))
+                            if value.day != -1 {
+                                Text("\(value.day)")
+                                    .font(.title3.bold())
+                                    .foregroundColor(isToday(date: value.date) ? Color(red: 0.42, green: 0.31, blue: 0.22) : Color(red: 0.42, green: 0.31, blue: 0.22))
+                                    .background(isToday(date: value.date) ? Color(red: 0.36, green: 0.25, blue: 0.20).opacity(0.5) : Color.clear)
+                                    .cornerRadius(10)
+                            } else {
+                                Text("")
+                                    .font(.title3.bold())
+                                    .foregroundColor(.clear)
+                            }
                         }
                     }
                 }
             }
         }
+        .onChange(of: currentMonth) { newValue in
+            currentDate = getCurrentMonth()
+        }
+    }
+    
+    // Year and month display
+    func extraDate() -> [String] {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MMMM"
+        
+        let date = formatter.string(from: getCurrentMonth())
+        
+        return date.components(separatedBy: " ")
+    }
+    
+    func getCurrentMonth() -> Date {
+        let calendar = Calendar.current
+        
+        guard let currentMonth = calendar.date(byAdding: .month, value: self.currentMonth, to: currentDate) else {
+            return Date()
+        }
+        return currentMonth
     }
     
     func extractDate() -> [DateValue] {
-        guard let currentMonth = Calendar.current.date(byAdding: .month, value: self.currentMonth, to: currentDate) else {
-            return []
-        }
+        let calendar = Calendar.current
+        let currentMonth = getCurrentMonth()
         
-        return currentMonth.getAllDates().compactMap { date -> DateValue in
-            let day = Calendar.current.component(.day, from: date)
+        var days = currentMonth.getAllDates().compactMap { date -> DateValue in
+            let day = calendar.component(.day, from: date)
             return DateValue(day: day, date: date)
         }
+        
+        let firstWeekday = calendar.component(.weekday, from: days.first?.date ?? Date())
+        
+        for _ in 0..<firstWeekday - 1 {
+            days.insert(DateValue(day: -1, date: Date()), at: 0)
+        }
+        
+        return days
+    }
+    
+    func isToday(date: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDateInToday(date)
     }
 }
 
@@ -106,7 +152,8 @@ extension Date {
         let range = calendar.range(of: .day, in: .month, for: startDate)!
         
         return range.compactMap { day -> Date in
-            return calendar.date(byAdding: .day, value: day - 1, to: startDate)!
+            calendar.date(byAdding: .day, value: day - 1, to: startDate)!
         }
     }
 }
+
