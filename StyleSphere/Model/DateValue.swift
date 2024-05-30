91d9233f9553f0625413c763c2fa17f595df394f
Nessaa16 <vpriscilia@student.ciputra.ac.id>
2024-05-30 14:16:43 +0700

    adding calendar view and add some logic (2)

---
+++ StyleSphere/Model/DateValue.swift
@@ -0,0 +1,15 @@
+//
+//  DataValue.swift
+//  StyleSphere
+//
+//  Created by MacBook Pro on 30/05/24.
+//
+
+import SwiftUI
+
+struct DataValue: Identifiable {
+    var id = UUID().uuidString
+    var day: Int
+    var date: Date
+}
+
