04b6a0eff8dccd663eee1b76c311d12100d0f06f
bridgetbtrxc <bbeatrix@student.ciputra.ac.id>
2024-06-01 16:12:31 +0700

    Add filtering color to the category detail
    has been able to show color but the bug is hasnt specifically shown each category color, will fix it soon

--- StyleSphere/StyleSphereApp.swift
+++ StyleSphere/StyleSphereApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct StyleSphereApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+            Navigation()
+        }.modelContainer(for: WardrobeItem.self)
     }
 }
---
+++ StyleSphere/ViewModel/WardrobeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  WardrobeViewModel.swift
+//  StyleSphere
+//
+//  Created by MacBook Pro on 29/05/24.
+//
+
+import Foundation
+import SwiftData
+
+class WardrobeViewModel: ObservableObject {
+//    @Query var wardrobeItems: [WardrobeItem]
+//    
+//    func addItem(category: String, name: String, color: String) {
+//        let newItem = WardrobeItem(category: category, name: name, color: color)
+//        ModelContext.main.insert(newItem)
+//    }
+//    
+//    func deleteItem(_ item: WardrobeItem) {
+//        ModelContext.main.delete(item)
+//    }
+}
