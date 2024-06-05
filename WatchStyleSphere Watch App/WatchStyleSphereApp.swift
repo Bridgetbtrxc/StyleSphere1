170129f69b28e769bc8d6e8b96603d018abd4fc7
bryananthony <bryananthony872@gmail.com>
2024-06-05 16:44:09 +0700

    added watchos

---
+++ WatchStyleSphere Watch App/WatchStyleSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchStyleSphereApp.swift
+//  WatchStyleSphere Watch App
+//
+//  Created by student on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchStyleSphere_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
