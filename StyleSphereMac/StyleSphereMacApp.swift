6aa56ed4d083a2b46005dabff86f948862089753
bridgetbtrxc <bbeatrix@student.ciputra.ac.id>
2024-06-07 00:14:36 +0700

    Revert "Mac device"
    This reverts commit edad82b37743eb838e698361adad7f3b92c6edbb.

---
+++ StyleSphereMac/StyleSphereMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleSphereMacApp.swift
+//  StyleSphereMac
+//
+//  Created by MacBook Pro on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleSphereMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
