bad038fca9f3fcd732cca40a973bc858cdbc1061
kimi <146687687+kimiharisen@users.noreply.github.com>
2024-06-06 17:05:39 +0700

    add watchos target & folders

--- StyleSphere/View/CalendarFix/CalendarViewController.swift
+++ StyleSphere/View/CalendarFix/CalendarViewController.swift
@@ -1,42 +1,42 @@
+////
+////  ViewController.swift
+////  StyleSphere
+////
+////  Created by MacBook Pro on 05/06/24.
+////
+//import UIKit
+//import CalendarKit
+//import EventKit
+//import SwiftUI
 //
-//  ViewController.swift
-//  StyleSphere
+//class CalendarViewController: DayViewController {
+//    private let eventStore = EKEventStore()
 //
-//  Created by MacBook Pro on 05/06/24.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        title = "Calendar"
+//        requestAccessToCalendar()
+//    }
 //
-import UIKit
-import CalendarKit
-import EventKit
-import SwiftUI
-
-class CalendarViewController: DayViewController {
-    private let eventStore = EKEventStore()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Calendar"
-        requestAccessToCalendar()
-    }
-
-    func requestAccessToCalendar(){
-        eventStore.requestAccess(to: .event) { success, error in
-
-        }
-    }
-}
-
-struct CalendarViewControllerRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = CalendarViewController
-
-    func makeUIViewController(context: Context) -> CalendarViewController {
-        return CalendarViewController()
-    }
-
-    func updateUIViewController(_ uiViewController: CalendarViewController, context: Context) {
-        // Update the view controller if needed
-    }
-}
-
-#Preview {
-    CalendarViewControllerRepresentable()
-}
+//    func requestAccessToCalendar(){
+//        eventStore.requestAccess(to: .event) { success, error in
+//
+//        }
+//    }
+//}
+//
+//struct CalendarViewControllerRepresentable: UIViewControllerRepresentable {
+//    typealias UIViewControllerType = CalendarViewController
+//
+//    func makeUIViewController(context: Context) -> CalendarViewController {
+//        return CalendarViewController()
+//    }
+//
+//    func updateUIViewController(_ uiViewController: CalendarViewController, context: Context) {
+//        // Update the view controller if needed
+//    }
+//}
+//
+//#Preview {
+//    CalendarViewControllerRepresentable()
+//}
